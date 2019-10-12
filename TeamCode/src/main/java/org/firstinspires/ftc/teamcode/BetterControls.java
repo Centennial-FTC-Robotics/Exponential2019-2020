ca069f8c0e235c10fcf384f31caa02e1acc6e807
Eric Zhu <ezhu2009@gmail.com>
2019-10-12 10:07:40 -0400

    ,jdsfghkbvwjdfhgkjsldh gkjhslk djf h,dfsh ,askj dhf,asmdf,masdnbfmnasbdfmnasdbfmadfsh

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BetterControls.java
@@ -0,0 +1,103 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@TeleOp(name = "TeleOp", group = "TeleOp")
+public class BetterControls extends LinearOpMode {
+    private DcMotor upleft;
+    private DcMotor backleft;
+    private DcMotor upright;
+    private DcMotor backright;
+
+    private double[] circle_to_taxicab(double circle_x, double circle_y) {
+        double[] answer = new double[2];
+        if (circle_x == 0.0)
+            answer[0] = 0.0;
+        else
+            answer[0] = circle_x / Math.abs(circle_x) * Math.sqrt(Math.pow(circle_x, 2) + Math.pow(circle_y, 2))
+                    * (Math.abs(circle_x)) / (Math.abs(circle_x) + Math.abs(circle_y));
+        if (circle_y == 0.0)
+            answer[1] = 0.0;
+        else
+            answer[1] = circle_y / Math.abs(circle_y) * Math.sqrt(Math.pow(circle_x, 2) + Math.pow(circle_y, 2))
+                    * (Math.abs(circle_y)) / (Math.abs(circle_x) + Math.abs(circle_y));
+        return answer;
+    }
+
+    private void taxicab_method(double powerright, double powerup) {
+        double taxiright = circle_to_taxicab(powerright, powerup)[0];
+        double taxiup = circle_to_taxicab(powerright, powerup)[1];
+        upleft.setPower((-taxiright + taxiup));
+        backleft.setPower((taxiright + taxiup));
+        upright.setPower((taxiright + taxiup));
+        backright.setPower((-taxiright + taxiup));
+    }
+
+    private void brute_force_method(double powerright, double powerup) {
+        double magnitude = Math.sqrt(powerright * powerright + powerup * powerup);
+        if (magnitude > 1) {
+            upleft.setPower((-powerright + powerup) / magnitude);
+            backleft.setPower((powerright + powerup) / magnitude);
+            upright.setPower((powerright + powerup) / magnitude);
+            backright.setPower((-powerright + powerup) / magnitude);
+        } else {
+            upleft.setPower((-powerright + powerup) / 2);
+            backleft.setPower((powerright + powerup) / 2);
+            upright.setPower((powerright + powerup) / 2);
+            backright.setPower((-powerright + powerup) / 2);
+        }
+    }
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        upleft = hardwareMap.dcMotor.get("upleft");
+        backleft = hardwareMap.dcMotor.get("backleft");
+        backright = hardwareMap.dcMotor.get("backright");
+        upright = hardwareMap.dcMotor.get("upright");
+
+        upleft.setDirection(DcMotorSimple.Direction.REVERSE);
+        backleft.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        upleft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backleft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        upright.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backright.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            double powerright = gamepad1.right_stick_x;
+            double powerup = gamepad1.right_stick_y;
+            double rotate_counterclockwise = gamepad1.left_stick_x;
+
+            double trigger_factor = 1.0 - gamepad1.left_trigger;
+            if (powerright != 0.0 || powerup != 0.0) {
+
+                // change this to adjust top speed
+                double adjustable_move_factor = 1.0;
+
+                // Comment either taxicab or brute_force out
+                taxicab_method(adjustable_move_factor * trigger_factor * powerright, adjustable_move_factor * trigger_factor * powerup);
+                // brute_force_method(adjustable_move_factor * trigger_factor * powerright, adjustable_move_factor * trigger_factor * powerup);
+            } else if (rotate_counterclockwise != 0.0) {
+                // change this to adjust rotation speed
+                double adjustable_rotate_factor = .5;
+
+                upleft.setPower(adjustable_rotate_factor * trigger_factor * -rotate_counterclockwise);
+                backleft.setPower(adjustable_rotate_factor * trigger_factor * -rotate_counterclockwise);
+                upright.setPower(adjustable_rotate_factor * trigger_factor * rotate_counterclockwise);
+                backright.setPower(adjustable_rotate_factor * trigger_factor * rotate_counterclockwise);
+            } else {
+                upleft.setPower(0);
+                backleft.setPower(0);
+                upright.setPower(0);
+                backright.setPower(0);
+            }
+        }
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Exponential_Hardware_Initializations.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+public abstract class Exponential_Hardware_Initializations extends LinearOpMode {
+    protected DcMotor frontLeft;
+    protected DcMotor frontRight;
+    protected DcMotor backLeft;
+    protected DcMotor backRight;
+    protected DcMotor intakeLeft;
+    protected DcMotor intakeRight;
+    protected DcMotor slideLeft;
+    protected DcMotor slideRight;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        frontLeft = hardwareMap.dcMotor.get("frontLeft");
+        frontRight = hardwareMap.dcMotor.get("frontRight");
+        backLeft = hardwareMap.dcMotor.get("backLeft");
+        backRight = hardwareMap.dcMotor.get("backRight");
+        intakeLeft = hardwareMap.dcMotor.get("intakeLeft");
+        intakeRight = hardwareMap.dcMotor.get("intakeRight");
+        slideLeft = hardwareMap.dcMotor.get("slideLeft");
+        slideRight = hardwareMap.dcMotor.get("slideRight");
+    }
+}
