41739ef487ca7ebff91f44fcc0d17ee4f020e2f2
mfang7 <margaretfang73@gmail.com>
2019-10-12 12:03:52 -0400

    removed wait for motors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Exponential_Methods.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Exponential_Methods.java
@@ -37,7 +37,7 @@ public abstract class Exponential_Methods extends  Exponential_Hardware_Initiali
         int forwardVal = convertInchToEncoder(forward);
         int rightVal = convertInchToEncoder(right);
 
-        for(DcMotor motor : driveMotors){
+        for(DcMotor motor : driveMotors) {
             motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
             motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
             motor.setPower(power);
