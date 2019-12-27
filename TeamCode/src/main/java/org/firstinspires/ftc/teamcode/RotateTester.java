56390d5eeb470291a6871f2f0f088618ff8a0a1f
Eric Zhu <ezhu2009@gmail.com>
2019-12-27 13:00:18 -0500

    Wrote Move and Turn testers that one can adjust for the angle/distance using the gamepad

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RotateTester.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RotateTester.java
@@ -3,8 +3,20 @@ package org.firstinspires.ftc.teamcode;
 public class RotateTester extends Exponential_Methods{
     public void runOpMode() throws InterruptedException {
         super.runOpMode();
-
-
+        waitForStart();
+        double rotateAngle = 0;
+        while(opModeIsActive()){
+            if(gamepad1.left_stick_x!=0){
+                rotateAngle+=10*gamepad1.left_stick_x;
+                wait(400);
+            }
+            if(gamepad1.a){
+                turnRelative(rotateAngle);
+            }
+            if(gamepad1.b){
+                turnRelative(rotateAngle);
+            }
+        }
     }
 }
 
