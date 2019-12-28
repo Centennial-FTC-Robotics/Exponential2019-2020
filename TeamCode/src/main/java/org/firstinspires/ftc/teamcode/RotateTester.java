cf24996616d088ae5a931e9b810f24152b753d61
Eric Zhu <ezhu2009@gmail.com>
2019-12-28 10:48:41 -0500

    Recalibrated rotate method with new p value

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RotateTester.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RotateTester.java
@@ -1,15 +1,20 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "RotateTester", group = "TeleOp")
 public class RotateTester extends Exponential_Methods{
     public void runOpMode() throws InterruptedException {
         super.runOpMode();
         waitForStart();
         double rotateAngle = 0;
         while(opModeIsActive()){
-            if(gamepad1.left_stick_x!=0){
+            if(gamepad1.left_stick_x!=0.0){
                 rotateAngle+=10*gamepad1.left_stick_x;
-                wait(400);
+                sleep(400);
             }
+            telemetry.addData("Angle", rotateAngle);
+            telemetry.update();
             if(gamepad1.a){
                 turnRelative(rotateAngle);
             }
