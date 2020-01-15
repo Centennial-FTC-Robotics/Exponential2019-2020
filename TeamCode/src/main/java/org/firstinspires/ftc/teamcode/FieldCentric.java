8876fd7fdd4ab7481563cec59343dca43e5bebb6
Eric Zhu <ezhu2009@gmail.com>
2020-01-14 19:32:45 -0500

    Had other two TeleOps extend TeleOpMethods

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FieldCentric.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FieldCentric.java
@@ -12,187 +12,38 @@ import org.firstinspires.ftc.robotcore.internal.tfod.Timer;
 
 @TeleOp(name = "FieldCentric", group = "TeleOp")
 
-public class FieldCentric extends Exponential_Methods {
-
-    public final static int SLIDE_MAX = slideMax - slideMin;
-    public final static int SLIDE_MIN = 0;
-    private double[] circle_to_taxicab(double circle_x, double circle_y, double circle_rotate) {
-        double[] answer = new double[4];
-        double x;
-        double y;
-
-        if (circle_x == 0.0) {
-            x = 0.0;
-        }else {
-            x = circle_x / Math.abs(circle_x) * Math.sqrt(Math.pow(circle_x, 2) + Math.pow(circle_y, 2))
-                    * (Math.abs(circle_x)) / (Math.abs(circle_x) + Math.abs(circle_y));
-        }
-        if (circle_y == 0.0) {
-            y = 0.0;
-        }else {
-            y = circle_y / Math.abs(circle_y) * Math.sqrt(Math.pow(circle_x, 2) + Math.pow(circle_y, 2))
-                    * (Math.abs(circle_y)) / (Math.abs(circle_x) + Math.abs(circle_y));
-        }
-        double sum = Math.abs(x)+Math.abs(y)+Math.abs(circle_rotate);
-        if(sum >1){
-            answer[0]=(x+y+circle_rotate)/sum;
-            answer[1]=(-x+y+circle_rotate)/sum;
-            answer[2]=(x+y-circle_rotate)/sum;
-            answer[3]=(-x+y-circle_rotate)/sum;
-        } else {
-            answer[0]=(x+y+circle_rotate);
-            answer[1]=(-x+y+circle_rotate);
-            answer[2]=(x+y-circle_rotate);
-            answer[3]=(-x+y-circle_rotate);
-        }
-        return answer;
-    }
-
+public class FieldCentric extends TeleOpMethods {
     @Override
-    public void runOpMode() throws InterruptedException {
-        super.runOpMode();
+    public void driveTrain() {
+        double currentAngle = getRotationinDimension('Z') + Math.PI / 2;
+        // double currentAngle = 0; //  TODO; FIND OUT HOW TO SET LATeR
 
+        //------FIELD CENTRIC-------
+        // THESE ANGLES ARE IN STANDARD POSITION. maybe non standard position works, but only if bearing bearing or std. std.
+        double inputX = gamepad1.left_stick_x;
+        double inputY = gamepad1.right_stick_y;
+        //gets the input angle. Adds pi to tangent if the x value of the angle is less than 0
+        double inputAngle = Math.atan(inputY / inputX) + inputX < 0 ? Math.PI : 0;
+        double inputMagnitude = Math.sqrt(inputX * inputX + inputY * inputY);
 
+        double angleDistance = inputAngle - currentAngle;
+        double angleOnRobot = currentAngle - angleDistance;  //angle of the movement relative to the robot's POV
 
-        for(DcMotor motor : driveMotors){
-            motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-            motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        double centricX = inputMagnitude * Math.cos(angleOnRobot);
+        double centricY = inputMagnitude * Math.sin(angleOnRobot);  //ask yuhwan for the picture if you want, it will not help
+
+        double[] answer = circle_to_taxicab(centricX, centricY, .8*gamepad1.right_stick_x);
+        double factor = 1;
+        if(gamepad1.left_bumper){
+            factor = .5;
         }
-        waitForStart();
-
-
-        int slidePosition = (slideUp.getCurrentPosition() + slideDown.getCurrentPosition())/2;
-        ElapsedTime timer = new ElapsedTime();
-
-        //-----------------------------------SLIDES------------------------------------
-        while (opModeIsActive()) {
-            telemetry.addData("SlideDown", slideDown.getCurrentPosition());
-            telemetry.addData("SlideUp", slideUp.getCurrentPosition());
-            telemetry.addData("SlidePosition", slidePosition);
-            telemetry.update();
-            if(gamepad2.left_stick_y!=0){
-                slideUp.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-                slideDown.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-                if(gamepad2.left_bumper){
-                    slideUp.setPower(.25*.5*-gamepad2.left_stick_y);
-                    slideDown.setPower(.25*.5*-gamepad2.left_stick_y);
-                } else if (gamepad2.right_bumper){
-                    slideUp.setPower(.5*.5*-gamepad2.left_stick_y);
-                    slideDown.setPower(.5*.5*-gamepad2.left_stick_y);
-                } else {
-                    slideUp.setPower((.5*-gamepad2.left_stick_y));
-                    slideDown.setPower((.5*-gamepad2.left_stick_y));
-                }
-                if(slidePosition > SLIDE_MAX) {
-                    if(gamepad2.left_stick_y>0){
-                        slideUp.setPower(.5*-gamepad2.left_stick_y);
-                        slideDown.setPower(.5*-gamepad2.left_stick_y);
-                    } else {
-                        setSlidePower(0);
-                    }
-                } else if(slidePosition < SLIDE_MIN)
-                    if(gamepad2.left_stick_y>0){
-                        slideUp.setPower(.25*.5*-gamepad2.left_stick_y);
-                        slideDown.setPower(.25*.5*-gamepad2.left_stick_y);
-                    } else {
-                        setSlidePower(0);
-                    }
-                else
-                    slidePosition = (slideDown.getCurrentPosition() + slideUp.getCurrentPosition()) / 2;
-            } else {
-                if(slidePosition > SLIDE_MAX)
-                    slidePosition = SLIDE_MAX;
-                else if(slidePosition < SLIDE_MIN)
-                    slidePosition = SLIDE_MIN;
-                slideUp.setTargetPosition(slidePosition);
-                slideDown.setTargetPosition(slidePosition);
-                slideUp.setPower(.3);
-                slideDown.setPower(.3);
-                slideUp.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                slideDown.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            }
-
-            //Make slides go up 4 inches
-            if(gamepad2.y&&timer.seconds()>.25){
-                slidePosition += convertInchToEncoderSlides(4);
-                timer = new ElapsedTime();
-            }
-            if(gamepad2.a&&timer.seconds()>.25){
-                slidePosition -= convertInchToEncoderSlides(4);
-                timer = new ElapsedTime();
-            }
-            //-----------------------------------MOVEMENT/SPEED------------------------------------
-
-            double bumper_factor = 1;
-            double left_trigger_factor = gamepad1.left_trigger;
-            double right_trigger_factor = gamepad1.right_trigger;
-
-            //double bumper_factor = 1.0 - gamepad1.left_trigger;
-            double currentAngle = getRotationinDimension('Z') + Math.PI / 2;
-            // double currentAngle = 0; //  TODO; FIND OUT HOW TO SET LATeR
-
-            //------FIELD CENTRIC-------
-            // THESE ANGLES ARE IN STANDARD POSITION. maybe non standard position works, but only if bearing bearing or std. std.
-            double inputX = gamepad1.left_stick_x;
-            double inputY = gamepad1.right_stick_y;
-            //gets the input angle. Adds pi to tangent if the x value of the angle is less than 0
-            double inputAngle = Math.atan(inputY / inputX) + inputX < 0 ? Math.PI : 0;
-            double inputMagnitude = Math.sqrt(inputX * inputX + inputY * inputY);
-
-            double angleDistance = inputAngle - currentAngle;
-            double angleOnRobot = currentAngle - angleDistance;  //angle of the movement relative to the robot's POV
-
-            double centricX = inputMagnitude * Math.cos(angleOnRobot);
-            double centricY = inputMagnitude * Math.sin(angleOnRobot);  //ask yuhwan for the picture if you want, it will not help
-
-            double[] answer = circle_to_taxicab(centricX, centricY, .8*gamepad1.right_stick_x);
-            if(gamepad1.left_bumper){
-                bumper_factor = .5;
-            }
-            if (gamepad1.right_bumper){
-                bumper_factor = .25;
-            }
-
-            frontRight.setPower(bumper_factor*answer[0]);
-            backRight.setPower(bumper_factor*answer[1]);
-            backLeft.setPower(bumper_factor*answer[2]);
-            frontLeft.setPower(bumper_factor*answer[3]);
-
-            //-----------------------------------HOOKS------------------------------------
-
-            //hook down
-            if(gamepad1.x&&timer.seconds()>.25) {
-                toggleHook(true);
-                timer = new ElapsedTime();
-            }
-            //hook up
-            if(gamepad1.b&&timer.seconds()>.25) {
-                toggleHook(false);
-                timer = new ElapsedTime();
-            }
-
-            //-----------------------------------INTAKE WHEELS------------------------------------
-            double intakePower = .9; //set later
-            setIntakeWheels(intakePower * gamepad2.left_trigger);
-            if(gamepad2.right_trigger!=0) {
-                setIntakeWheels(-.5 * intakePower * gamepad2.right_trigger);
-            }
-
-            //-----------------------------------INTAKE ARMS------------------------------------
-            //Intake arm servos
-            if(gamepad2.b&&timer.seconds()>.25){
-                intakeServoLeft.setPosition(.62);
-                intakeServoRight.setPosition(.92);
-                timer = new ElapsedTime();
-            }
-
-            if(gamepad2.x&&timer.seconds()>.25){
-                //intake servos release stone
-                intakeServoLeft.setPosition(.55);
-                intakeServoRight.setPosition(.85);
-                timer = new ElapsedTime();
-            }
+        if (gamepad1.right_bumper){
+            factor = .25;
         }
+        frontRight.setPower(factor*answer[0]);
+        backRight.setPower(factor*answer[1]);
+        backLeft.setPower(factor*answer[2]);
+        frontLeft.setPower(factor*answer[3]);
+
     }
-
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpDriverSlidesStillUp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpDriverSlidesStillUp.java
@@ -12,162 +12,63 @@ import org.firstinspires.ftc.robotcore.internal.tfod.Timer;
 
 @TeleOp(name = "teleop slides still up", group = "TeleOp")
 
-public class TeleOpDriverSlidesStillUp extends Exponential_Methods {
+public class TeleOpDriverSlidesStillUp extends TeleOpMethods {
 
     public final static int SLIDE_MAX = slideMax;
     public final static int SLIDE_MIN = slideMin;
-    private double[] circle_to_taxicab(double circle_x, double circle_y, double circle_rotate) {
-        double[] answer = new double[4];
-        double x;
-        double y;
-
-        if (circle_x == 0.0) {
-            x = 0.0;
-        }else {
-            x = circle_x / Math.abs(circle_x) * Math.sqrt(Math.pow(circle_x, 2) + Math.pow(circle_y, 2))
-                    * (Math.abs(circle_x)) / (Math.abs(circle_x) + Math.abs(circle_y));
-        }
-        if (circle_y == 0.0) {
-            y = 0.0;
-        }else {
-            y = circle_y / Math.abs(circle_y) * Math.sqrt(Math.pow(circle_x, 2) + Math.pow(circle_y, 2))
-                    * (Math.abs(circle_y)) / (Math.abs(circle_x) + Math.abs(circle_y));
-        }
-        double sum = Math.abs(x)+Math.abs(y)+Math.abs(circle_rotate);
-        if(sum >1){
-            answer[0]=(x+y+circle_rotate)/sum;
-            answer[1]=(-x+y+circle_rotate)/sum;
-            answer[2]=(x+y-circle_rotate)/sum;
-            answer[3]=(-x+y-circle_rotate)/sum;
-        } else {
-            answer[0]=(x+y+circle_rotate);
-            answer[1]=(-x+y+circle_rotate);
-            answer[2]=(x+y-circle_rotate);
-            answer[3]=(-x+y-circle_rotate);
-        }
-        return answer;
-    }
 
     @Override
-    public void runOpMode() throws InterruptedException {
-        super.runOpMode();
-
-
-
-        for(DcMotor motor : driveMotors){
-            motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-            motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        }
-        waitForStart();
-
-
+    public void slideMotors() {
         int slidePosition = (slideUp.getCurrentPosition() + slideDown.getCurrentPosition())/2;
-        ElapsedTime timer = new ElapsedTime();
-
-        while (opModeIsActive()) {
-            telemetry.addData("SlideDown", slideDown.getCurrentPosition());
-            telemetry.addData("SlideUp", slideUp.getCurrentPosition());
-            telemetry.addData("SlidePosition", slidePosition);
-            telemetry.update();
-            if(gamepad2.left_stick_y!=0){
-                slideUp.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-                slideDown.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-                if(gamepad2.left_bumper){
-                    slideUp.setPower(.25*.5*-gamepad2.left_stick_y);
-                    slideDown.setPower(.25*.5*-gamepad2.left_stick_y);
-                } else if (gamepad2.right_bumper){
-                    slideUp.setPower(.5*.5*-gamepad2.left_stick_y);
-                    slideDown.setPower(.5*.5*-gamepad2.left_stick_y);
-                } else {
-                    slideUp.setPower((.5*-gamepad2.left_stick_y));
-                    slideDown.setPower((.5*-gamepad2.left_stick_y));
-                }
-                if(slidePosition > SLIDE_MAX) {
-                    if(gamepad2.left_stick_y>0){
-                        slideUp.setPower(.5*-gamepad2.left_stick_y);
-                        slideDown.setPower(.5*-gamepad2.left_stick_y);
-                    } else {
-                        setSlidePower(0);
-                    }
-                } else if(slidePosition < SLIDE_MIN)
-                    if(gamepad2.left_stick_y>0){
-                        slideUp.setPower(.25*.5*-gamepad2.left_stick_y);
-                        slideDown.setPower(.25*.5*-gamepad2.left_stick_y);
-                    } else {
-                        setSlidePower(0);
-                    }
-                else
-                    slidePosition = (slideDown.getCurrentPosition() + slideUp.getCurrentPosition()) / 2;
+        if(gamepad2.left_stick_y!=0){
+            slideUp.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            slideDown.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            if(gamepad2.left_bumper){
+                slideUp.setPower(SLIDE_FACTOR*LEFT_BUMPER_TRIGGER_FACTOR*-gamepad2.left_stick_y);
+                slideDown.setPower(SLIDE_FACTOR*LEFT_BUMPER_TRIGGER_FACTOR*-gamepad2.left_stick_y);
+            } else if (gamepad2.right_bumper){
+                slideUp.setPower(RIGHT_BUMPER_TRIGGER_FACTOR*SLIDE_FACTOR*-gamepad2.left_stick_y);
+                slideDown.setPower(RIGHT_BUMPER_TRIGGER_FACTOR*SLIDE_FACTOR*-gamepad2.left_stick_y);
             } else {
-                if(slidePosition > SLIDE_MAX)
-                    slidePosition = SLIDE_MAX;
-                else if(slidePosition < SLIDE_MIN)
-                    slidePosition = SLIDE_MIN;
-                slideUp.setTargetPosition(slidePosition);
-                slideDown.setTargetPosition(slidePosition);
-                slideUp.setPower(.3);
-                slideDown.setPower(.3);
-                slideUp.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                slideDown.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                slideUp.setPower((SLIDE_FACTOR*-gamepad2.left_stick_y));
+                slideDown.setPower((SLIDE_FACTOR*-gamepad2.left_stick_y));
             }
-            double bumper_factor = 1;
-            double left_trigger_factor = gamepad1.left_trigger;
-            double right_trigger_factor = gamepad1.right_trigger;
-
-            //double bumper_factor = 1.0 - gamepad1.left_trigger;
-            double[] answer = circle_to_taxicab(gamepad1.left_stick_x, gamepad1.left_stick_y, .8*gamepad1.right_stick_x);
-            if(gamepad1.left_bumper){
-                bumper_factor = .5;
-            }
-            if (gamepad1.right_bumper){
-                bumper_factor = .25;
-            }
-
-            frontRight.setPower(bumper_factor*answer[0]);
-            backRight.setPower(bumper_factor*answer[1]);
-            backLeft.setPower(bumper_factor*answer[2]);
-            frontLeft.setPower(bumper_factor*answer[3]);
-
-            //hook down
-            if(gamepad1.x&&timer.seconds()>.25) {
-                toggleHook(true);
-                timer = new ElapsedTime();
-            }
-            //hook up
-            if(gamepad1.b&&timer.seconds()>.25) {
-                toggleHook(false);
-                timer = new ElapsedTime();
-            }
-
-            double intakePower = .9; //set later
-            setIntakeWheels(intakePower * gamepad2.left_trigger);
-            if(gamepad2.right_trigger!=0) {
-                setIntakeWheels(-.25 * intakePower * gamepad2.right_trigger);
-            }
-            //Make slides go up 4 inches
-            if(gamepad2.y&&timer.seconds()>.25){
-                slidePosition += convertInchToEncoderSlides(4);
-                timer = new ElapsedTime();
-            }
-            if(gamepad2.a&&timer.seconds()>.25){
-                slidePosition -= convertInchToEncoderSlides(4);
-                timer = new ElapsedTime();
-            }
-
-            //Intake arm servos
-            if(gamepad2.b&&timer.seconds()>.25){
-                intakeServoLeft.setPosition(.62);
-                intakeServoRight.setPosition(.92);
-                timer = new ElapsedTime();
-            }
-
-            if(gamepad2.x&&timer.seconds()>.25){
-                //intake servos release stone
-                intakeServoLeft.setPosition(.55);
-                intakeServoRight.setPosition(.85);
-                timer = new ElapsedTime();
-            }
+            if(slidePosition > SLIDE_MAX) {
+                if(gamepad2.left_stick_y>0){
+                    slideUp.setPower(SLIDE_FACTOR*-gamepad2.left_stick_y);
+                    slideDown.setPower(SLIDE_FACTOR*-gamepad2.left_stick_y);
+                } else {
+                    setSlidePower(0);
+                }
+            } else if(slidePosition < SLIDE_MIN)
+                if(gamepad2.left_stick_y>0){
+                    slideUp.setPower(SLIDE_FACTOR*-gamepad2.left_stick_y);
+                    slideDown.setPower(SLIDE_FACTOR*-gamepad2.left_stick_y);
+                } else {
+                    setSlidePower(0);
+                }
+            //else
+            //slidePosition = (slideDown.getCurrentPosition() + slideUp.getCurrentPosition()) / 2;
+        } else {
+            if(slidePosition > SLIDE_MAX)
+                slidePosition = SLIDE_MAX;
+            else if(slidePosition < SLIDE_MIN)
+                slidePosition = SLIDE_MIN;
+            slideUp.setTargetPosition(slidePosition);
+            slideDown.setTargetPosition(slidePosition);
+            slideUp.setPower(SLIDE_POWER);
+            slideDown.setPower(SLIDE_POWER);
+            slideUp.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            slideDown.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        }
+        if(gamepad2.y&&timer.seconds()>TIMER_INTERVAL){
+            slidePosition += convertInchToEncoderSlides(4);
+            timer.reset();
+        }
+        if(gamepad2.a&&timer.seconds()>TIMER_INTERVAL){
+            slidePosition -= convertInchToEncoderSlides(4);
+            timer.reset();
         }
     }
-
+    }
 }
