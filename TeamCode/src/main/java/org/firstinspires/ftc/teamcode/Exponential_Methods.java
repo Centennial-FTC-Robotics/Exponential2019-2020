be8d3d9af13b06d416b2ee5e3f71b74a2a4be4de
carl625 <carl625@gmail.com>
2020-02-18 14:37:25 -0500

    qualifier shit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Exponential_Methods.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Exponential_Methods.java
@@ -523,11 +523,6 @@ public abstract class Exponential_Methods extends Exponential_Hardware_Initializ
 
         int val = position + encoderVal;
 
-        if(position > slideMax)
-            val = slideMax;
-        else if(position < slideMin)
-            val = slideMin;
-
         slideUp.setTargetPosition(val);
         slideDown.setTargetPosition(val);
         slideUp.setMode(DcMotor.RunMode.RUN_TO_POSITION);
@@ -697,7 +692,7 @@ public abstract class Exponential_Methods extends Exponential_Hardware_Initializ
         //int numBlocks = stonePos;
         //int numBlocks = left ? 0 : center ? 1 : right ? 2: -1;
         int inchesBlocks = stonePos * 8;
-        double intakeOffset = TILE_LENGTH - ROBOT_LENGTH; //TODO: change the number later, inches to get the robot close enough to the block
+        double intakeOffset = TILE_LENGTH - ROBOT_LENGTH/* + 1*/; //TODO: change the number later, inches to get the robot close enough to the block
 
         //outwardsIntake();
 
@@ -742,7 +737,7 @@ public abstract class Exponential_Methods extends Exponential_Hardware_Initializ
         extendSlidesBy(-12, .5);
         outwardsIntake();
 
-        move(0, -11);
+        move(0, -11, .3);
         sleep(500);
         toggleHook(true);
         sleep(1000);
@@ -987,8 +982,13 @@ public abstract class Exponential_Methods extends Exponential_Hardware_Initializ
         else
             factor = -1;
 
-        bringSlidesDown();
+        //bringSlidesDown();
         move(factor * (TILE_LENGTH / 2), 0, .5);
+        //outwardsIntake();
+        extendSlidesBy(2,0.5);
+        sleep(500);
         outwardsIntake();
+        sleep(500);
+        extendSlidesTo(slideMin, .5);
     }
 }
