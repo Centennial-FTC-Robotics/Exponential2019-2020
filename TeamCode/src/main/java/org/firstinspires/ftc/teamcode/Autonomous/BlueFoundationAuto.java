009e674c8768ee3e991901b4d63b98463e2c1dc4
carl625 <carl625@gmail.com>
2020-02-23 15:29:38 -0500

    auto shit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/BlueFoundationAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/BlueFoundationAuto.java
@@ -4,7 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
 import org.firstinspires.ftc.teamcode.Exponential_Methods;
 
-@Autonomous(group = "Autonomous", name = "Blue Foundation")
+//@Autonomous(group = "Autonomous", name = "Blue Foundation")
 public class BlueFoundationAuto extends AutonomousPaths {
     public void runOpMode() throws InterruptedException { // starts on third tile from the right
         super.runOpMode();
