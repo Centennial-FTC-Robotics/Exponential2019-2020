48d15a5eb42a49bfd52de8a80938ba1e0c6dc53a
carl625 <carl625@gmail.com>
2020-02-27 15:05:04 -0500

    pull god help me

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Trash/MoveToTester.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Trash/MoveToTester.java
@@ -10,38 +10,40 @@ public class MoveToTester extends AutonomousPaths {
     public void runOpMode() throws InterruptedException {
         super.runOpMode();
         waitForStart();
-
+        initialHeading -= 90;
         currentPosition = new Position(0, 0);
 
         //top right -> top left -> bottom right -> origin
-        moveTo(12, 12);
-        moveTo(-12, 12);
-        moveTo(12, -12);
+
+        double inches = 6;
+        moveTo(inches, inches);
+        moveTo(-inches, inches);
+        moveTo(inches, -inches);
         moveTo(0, 0);
         sleep(1500);
 
         turnRelative(-90);
-        moveTo(12, 12);
-        moveTo(-12, 12);
-        moveTo(12, -12);
+        moveTo(inches, inches);
+        moveTo(-inches, inches);
+        moveTo(inches, -inches);
         moveTo(0, 0);
         sleep(1500);
 
         turnRelative(-45);
-        moveTo(12, 12);
-        moveTo(-12, 12);
-        moveTo(12, -12);
+        moveTo(inches, inches);
+        moveTo(-inches, inches);
+        moveTo(inches, -inches);
         moveTo(0, 0);
         sleep(1500);
 
         turnRelative(180);
-        moveTo(12, 12);
-        moveTo(-12, 12);
-        moveTo(12, -12);
+        moveTo(inches, inches);
+        moveTo(-inches, inches);
+        moveTo(inches, -inches);
         moveTo(0, 0);
-        /*moveTo(12, 12);
-        moveTo(0, 12);
-        moveTo(-12, -12);
+        /*moveTo(inches, inches);
+        moveTo(0, inches);
+        moveTo(-inches, -inches);
         moveTo(0, 0);*/
 
     }
