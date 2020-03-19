3241c5170ceec86cd1afd90d6a655af85b136b99
Eric Zhu <ezhu2009@gmail.com>
2020-03-19 19:39:15 -0400

    Diego test this

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/initialAngleOrientation180.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+@TeleOp(name = "Starting Angle 180")
+public class initialAngleOrientation180 extends Exponential_Methods {
+    public void runOpMode() throws InterruptedException{
+        super.runOpMode();
+        frontLeft.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.FLOAT);
+        frontRight.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.FLOAT);
+        backLeft.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.FLOAT);
+        backRight.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.FLOAT);
+        odoWheelForwards.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        odoWheelSideways.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        odoWheelForwards.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        odoWheelSideways.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+
+        decay.startingAngle = 180;
+        while(opModeIsActive()){
+            decay.updateRobot();
+            telemetry.addData("X coord", convertEncoderToInchOdom(decay.currentX));
+            telemetry.addData("Y coord", convertEncoderToInchOdom(decay.currentY));
+            telemetry.update();
+        }
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/intialAngleOrientation45.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+import org.firstinspires.ftc.teamcode.Exponential_Methods;
+
+
+@TeleOp(name = "Starting Angle 45")
+public class intialAngleOrientation45 extends Exponential_Methods {
+    public void runOpMode() throws InterruptedException{
+        super.runOpMode();
+        frontLeft.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.FLOAT);
+        frontRight.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.FLOAT);
+        backLeft.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.FLOAT);
+        backRight.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.FLOAT);
+        odoWheelForwards.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        odoWheelSideways.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        odoWheelForwards.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        odoWheelSideways.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+
+        decay.startingAngle = 45;
+        while(opModeIsActive()){
+            decay.updateRobot();
+            telemetry.addData("X coord", convertEncoderToInchOdom(decay.currentX));
+            telemetry.addData("Y coord", convertEncoderToInchOdom(decay.currentY));
+            telemetry.update();
+        }
+    }
+}
